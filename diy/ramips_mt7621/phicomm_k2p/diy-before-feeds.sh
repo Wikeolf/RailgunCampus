93abb9b07acd3146f13bfe72b38e0de507744f50
Wikeolf <Wikeolf@Gmail.com>
2023-12-23 20:10:27 +0800

    update: diy-before-feeds.sh add mosdns

--- diy/ramips_mt7621/phicomm_k2p/diy-before-feeds.sh
+++ diy/ramips_mt7621/phicomm_k2p/diy-before-feeds.sh
@@ -11,3 +11,10 @@ function git_sparse_clone() {
 
 # Add mtkiappd support for 802.11 k/v/r
 git_sparse_clone https://github.com/coolsnowwolf/lede lede package/lean/mt/mtkiappd package/kernel/mt-drivers/mtkiappd
+
+# Add luci-app-mosdns for diy dns server
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
